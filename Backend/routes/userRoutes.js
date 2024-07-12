9074238b316432d82d17ead43383c00c27c14c34
Manuu201 <manuel.vargasv@usm.cl>
2024-07-12 17:37:39 -0400

    CRUD user completado

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -11,4 +11,71 @@ router.post('/', async (req, res) => {
   }
 });
 
+router.put('/:id', async (req, res) => {
+    const userId = req.params.id;
+    try {
+      let user = await User.findByPk(userId);
+      if (!user) {
+        return res.status(404).json({ error: 'Usuario no encontrado' });
+      }
+      // Actualizar los datos del usuario
+      user = await user.update(req.body);
+      res.json(user);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
+
+router.delete('/:id', async (req, res) => {
+  const userId = req.params.id;
+  try {
+    const user = await User.findByPk(userId);
+    if (!user) {
+      return res.status(404).json({ error: 'Usuario no encontrado' });
+    }
+    await user.destroy();
+    res.json({ message: 'Usuario eliminado correctamente' });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.patch('/:id', async (req, res) => {
+    const userId = req.params.id;
+    try {
+      let user = await User.findByPk(userId);
+      if (!user) {
+        return res.status(404).json({ error: 'Usuario no encontrado' });
+      }
+      // Actualizar solo los campos especificados en req.body
+      user = await user.update(req.body);
+      res.json(user);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
+  
+  router.get('/', async (req, res) => {
+    try {
+      const users = await User.findAll();
+      res.json(users);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+
+  router.get('/:id', async (req, res) => {
+    const userId = req.params.id;
+    try {
+      const user = await User.findByPk(userId);
+      if (!user) {
+        return res.status(404).json({ error: 'Usuario no encontrado' });
+      }
+      res.json(user);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+
 module.exports = router;
+
